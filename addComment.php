e8cdc4bffc2984275f2677d9606af7a712f5f885
chris <chris@dickens.galagonwagon.com>
2011-04-17 22:48:27 +0000

    added stuff to comments portion

--- addComment.php
+++ addComment.php
@@ -1,6 +1,19 @@
 <?php 
 require_once(dirname(__FILE__).'/lib/common.inc.php');
-$comment = $_POST['comment'];
+
+$comment = new Comment();
+$today = date('Y-m-d');
+$keydate = date('Ymd');
+$postKey = md5($_POST["comment"]).'_'.$keydate;
+
+#Check the $_POST to take action
+if($_POST){
+  if (!isset($_POST["comment"]) OR empty($_POST["comment"])){
+    $errors[] = "Comment body  wasn't submitted";
+  }
+#if there are no errors store the post data
+if(!isset($errors)){
+    
+}
 
 ?>
-
---
+++ lib/comments.inc.php
@@ -0,0 +1,31 @@
+<?php
+class Comment {
+
+	private $riak;
+
+	public $bucket;
+	public $post;
+
+	function __construct(){
+		$riak_connection = new Riak();
+		$this->riak = $riak_connection->client;
+		$this->bucket = $this->riak->bucket('comments');
+	}
+
+	function get_comments(){
+		$keys = $this->bucket->getKeys();
+		return $keys;
+	}
+	function get_comment($key){
+		$this->post = $this->bucket->get($key);
+		return $this->post->getData();
+	}
+	function store_comment($key, $data){
+		$post = $this->bucket->newObject($key, $data);
+		$post -> store();
+	}
+	function delete_comment($key){
+		$post = $this->bucket->get($key);
+		$post->delete();
+	}
+}

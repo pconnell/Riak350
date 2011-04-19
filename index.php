8602361f09cb56b5d24b98c29a0e7c3011648ccc
Patrick <pconnell89@gmail.com>
2011-04-19 13:48:02 -0700

    Edited index.php via GitHub

--- index.php
+++ index.php
@@ -13,8 +13,30 @@ beginPage();
 <h1>Blog Site</h1>
 
 <h2>Recent Posts</h2>
-(Patrick)
-
+<?php
+        $posts = new Post();
+        $post_keys = $posts->get_posts();
+        $posts_data = array();
+        foreach($post_keys as $key){
+                $posts_data[$key] = $posts->get_post($key);
+        }
+        echo "<form method = 'get' action = 'viewPost.php'>";
+        foreach ($posts_data as $key => $post){
+                $title = $post['title'];
+                $date = $post['date'];
+                if ($title == null){
+                        $title = "No Title given";
+                }
+                if ($date == null){
+                        $date = "no date assigned";
+                }
+                echo "<a href = 'viewPost.php?key=$key'>
+                        <strong><big>$title</big></strong><br/>
+                        on: <em>$date</em><br/><br/>
+                      </a>";
+        }
+        echo "</form>";
+?>
 <h2>Popular Tags</h2>
 <?php foreach (array_keys($tags_data) as $tag_key) { ?>
 <a href="viewTag.php?tag=<?php echo $tag_key?>"><?php echo $tag_key?></a><br/>

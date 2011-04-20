df4ef1a1c95736a07ae75d9703f16fcc120f0983
Aaron <aclemmer@mail.umw.edu>
2011-04-20 20:27:56 +0000

    added menu, some formatting fixes

--- lib/master.php
+++ lib/master.php
@@ -1,10 +1,14 @@
 <html>
 <body>
-          <div class="content">
-	  <h2 class="title"><?php echo $SITE_PAGE_TITLE?></h2>
-            <div class="inner">
-              <?php echo $SITE_MAIN ?>
-            </div>
-          </div>
+  <div class="header">
+    <a href="index.php">Home</a> |
+    <a href="addPost.php">Add a Post</a>
+  </div>
+  <div class="content">
+  <h2 class="title"><?php echo $SITE_PAGE_TITLE?></h2>
+    <div class="inner">
+      <?php echo $SITE_MAIN ?>
+    </div>
+  </div>
 </body>
 </html>
